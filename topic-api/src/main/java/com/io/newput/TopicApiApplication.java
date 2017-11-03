d1424019fff5e46d69bd073847f3c7133ac5f7f2
rahulkulmi <rahul@newput.com>
2017-11-03 13:43:39 +0530

    First commit for demo project.

---
+++ topic-api/src/main/java/com/io/newput/TopicApiApplication.java
@@ -0,0 +1,12 @@
+package com.io.newput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicApiApplication.class, args);
+	}
+}
